cc58153d1ed6063b7f0c6282c5b4128595a79edf
David Venegas <juandavidcelis10@gmail.com>
2025-03-17 23:58:25 -0500

    layoutsFinished

--- projectNativas/app/build.gradle.kts
+++ projectNativas/app/build.gradle.kts
@@ -33,6 +33,7 @@ android {
 
 dependencies {
 
+    implementation("com.github.PhilJay:MPAndroidChart:3.1.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
